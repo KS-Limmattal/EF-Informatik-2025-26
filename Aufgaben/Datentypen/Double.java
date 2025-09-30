4fc3b7dd07e940fffba8ff3a380c09a0150a4548
Jane Doe <janedoe@example.com>
2025-09-30 15:46:12 +0200

    move Datentypen handout and exercises

---
+++ Aufgaben/Datentypen/Double.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.TimeUnit;
+
+public class Double {
+    public static void main(String[] args) throws InterruptedException {
+        // Problem 1
+        // double d = 1. / 2;
+        // System.out.println(d);
+
+        // Problem 2
+        double sum = 0;
+        double summand = 0.1;
+        while (sum < 1) {
+            System.out.println(sum);
+            sum = sum + summand;
+            TimeUnit.MILLISECONDS.sleep(1000);
+        }
+
+    }
+}
